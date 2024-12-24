787d8d58414d773d4be914820147856d5787b9e6
esansano <esansano@uji.es>
2024-12-24 11:10:07 +0100

    first commit

--- evaluate_accuracy.py
+++ evaluate_accuracy.py
@@ -5,7 +5,7 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 
 
 def run_evaluation():
-    models = c.BEST_MODELS
+    models = c.ALL_MODELS
     datasets = c.DATASETS
     params = {'batch_size': 16, 'print_predictions': True, 'num_workers': 10}
     Evaluate()(models, datasets, **params)
